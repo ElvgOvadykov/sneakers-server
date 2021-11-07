ed22692c2d7f9320fa2f0834638b120586dd2324
Овадыков Элвг Хонгорович <eovadykov@vtb.ru>
2021-11-07 17:23:29 +0300

    Init commit

---
+++ router/index.js
@@ -0,0 +1,19 @@
+const Router = require("express").Router;
+const UserController = require("../controllers/userController");
+const router = new Router();
+const { body } = require("express-validator");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+router.post(
+  "/sign-up",
+  body("email").isEmail(),
+  body("password").isLength({ min: 3, max: 32 }),
+  UserController.signUp
+);
+router.post("/login", UserController.login);
+router.post("/logout", UserController.logout);
+router.get("/activate/:link", UserController.activate);
+router.get("/refresh", UserController.refresh);
+router.get("/users", authMiddleware, UserController.getUsers);
+
+module.exports = router;
